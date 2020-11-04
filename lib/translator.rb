44791a1ead26c76796ef8aaff3e4a27898aa76ad
Claire DeBoer <claire.deboer@gmail.com>
2020-11-04 21:59:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,11 @@ end
 def get_japanese_emoticon(file_path, english_emoticon)
   # code goes here
   emoticons = load_library(file_path)
+<<<<<<< HEAD
   emoticons.each do |emotion, values_hash|
+=======
+  emoticons.each do |key, values_hash|
+>>>>>>> 227bbf9de947ffc8193d802b7b9194f2c892aca8
     if english_emoticon == values_hash[:english]
       return values_hash[:japanese]
     end
@@ -28,9 +32,16 @@ end
 def get_english_meaning(file_path, japanese_emoticon)
   # code goes here
   emoticons = load_library(file_path)
+<<<<<<< HEAD
   emoticons.each do |emotion, values_hash|
     if japanese_emoticon == values_hash[:japanese]
       return emotion
+=======
+  emoticons.each do |key, values_hash|
+    if japanese_emoticon == values_hash[:japanese]
+      binding.pry
+      return key
+>>>>>>> 227bbf9de947ffc8193d802b7b9194f2c892aca8
     end
   end
   "Sorry, that emoticon was not found"
